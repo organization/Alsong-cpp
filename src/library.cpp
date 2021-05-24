338a49b26fbd7264eadff482e5f6d9a0bc681262
JellyBrick <shlee1503@naver.com>
2021-05-25 04:56:37 +0900

    fix: build

--- src/library.cpp
+++ src/library.cpp
@@ -1,16 +1,21 @@
 #include <alsongpp/alsong.h>
-#include <alsongpp/boost/split.hpp>
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 
+#include <openssl/bn.h>
+#include <openssl/rsa.h>
+
 #include <date/tz.h>
 #include <httplib.h>
-#include <mbedcrypto/rsa.hpp>
-#include <mbedtls/bignum.h>
-#include <simdjson.h>
+#include <nlohmann/json.hpp>
 
 #include <algorithm>
 #include <regex>
+
+#define ALKEY "dfbc1f3f4c10e17e0112d72e78916da506edd57da06eac6ae4f00dd301067178"\
+              "057baa9ba94ef6e665bfb29cee567de4081249c0be376f9811383ce6d12bad74"\
+              "4a2f12fc16189c3d6ec041222b45954184165f37d98d188ed5ad158ff8b5004e"\
+              "8e717f714fc962ab7eb02d58481960d4d62f09c0b642e496ec703eca1c65374b"
 
 namespace alsongpp {
     constexpr auto alsong_base_url = "https://lyric.altools.com";
@@ -52,16 +57,13 @@ namespace alsongpp {
         form_data_items.emplace_back(enc_data_form);
         auto result = http_client.Post("/v1/search", form_data_items)->body;
 
-        simdjson::ondemand::parser parser;
-        simdjson::padded_string json = result;
-        auto json_result = parser.iterate(json);
-        auto arr = json_result.get_array();
+        nlohmann::json json = result;
 
         std::vector<SearchResult> searchResults;
-        for (auto element : arr) {
+        for (auto element : json) {
             SearchResult search_result{
-                    .lyric_id = element["lyric_id"].get_int64(),
-                    .playtime = element["playtime"].get_int64(),
+                    .lyric_id = element["lyric_id"].get<int64_t>(),
+                    .playtime = element["playtime"].get<int64_t>(),
                     .artist = to_string(element["artist"]),
                     .album = to_string(element["album"]),
                     .title = to_string(element["title"]),
@@ -71,10 +73,15 @@ namespace alsongpp {
         return searchResults;
     }
 
-    std::map<int64_t, std::vector<std::string>> parse_lyric(std::string_view lyric) {
+    std::map<int64_t, std::vector<std::string>> parse_lyric(std::string lyric) {
         std::map<int64_t, std::vector<std::string>> result;
+        size_t pos;
         std::vector<std::string> lyric_vec;
-        boost::split(lyric_vec, lyric, "<br>");
+        std::string del = "<br>";
+        while ((pos = lyric.find(del)) != std::string::npos) {
+            lyric_vec.emplace_back(lyric.substr(0, pos));
+            lyric.erase(0, pos + del.length());
+        }
         static std::regex reg(R"(^\[(\d+):(\d\d).(\d\d)\](.*)$)");
         for (const auto& element : lyric_vec) {
             std::smatch match;
@@ -106,15 +113,13 @@ namespace alsongpp {
         form_data_items.emplace_back(enc_data_form);
         auto result = http_client.Post("/v1/info", form_data_items)->body;
 
-        simdjson::ondemand::parser parser;
-        simdjson::padded_string json = result;
-        auto json_result = parser.iterate(json);
+        nlohmann::json json_result = result;
 
         LyricInfo lyric_info{
                 .register_url = to_string(json_result["register_url"]),
                 .lyrics = parse_lyric(to_string(json_result["lyric"])),
                 .album_name = to_string(json_result["album_name"]),
-                .info_id = json_result["info_id"].get_int64(),
+                .info_id = json_result["info_id"].get<int64_t>(),
                 .register_first_email = to_string(json_result["register_first_email"]),
                 .register_comment = to_string(json_result["register_comment"]),
                 .register_name = to_string(json_result["register_name"]),
@@ -147,18 +152,16 @@ namespace alsongpp {
         form_data_items.emplace_back(enc_data_form);
         auto result = http_client.Post("/v1/lookup", form_data_items)->body;
 
-        simdjson::ondemand::parser parser;
-        simdjson::padded_string json = result;
-        auto json_result = parser.iterate(json);
+        nlohmann::json json_result = result;
 
         LyricLookup lyric_info{
                 .register_url = to_string(json_result["register_url"]),
                 .lyrics = parse_lyric(to_string(json_result["lyric"])),
                 .album_name = to_string(json_result["album_name"]),
-                .info_id = json_result["info_id"].get_int64(),
-                .count_good = json_result["count_good"].get_int64(),
-                .count_bad = json_result["count_bad"].get_int64(),
-                .status_id = json_result["status_id"].get_int64(),
+                .info_id = json_result["info_id"].get<int64_t>(),
+                .count_good = json_result["count_good"].get<int64_t>(),
+                .count_bad = json_result["count_bad"].get<int64_t>(),
+                .status_id = json_result["status_id"].get<int64_t>(),
                 .register_first_email = to_string(json_result["register_first_email"]),
                 .register_comment = to_string(json_result["register_comment"]),
                 .register_name = to_string(json_result["register_name"]),
@@ -187,33 +190,49 @@ namespace alsongpp {
         form_data_items.emplace_back(mureka_id_form);
         auto result = http_client.Post("/v1/lookupListByMurekaId", form_data_items)->body;
 
-        simdjson::ondemand::parser parser;
-        simdjson::padded_string json = result;
-        auto json_result = parser.iterate(json);
-        auto arr = json_result.get_array();
+        nlohmann::json json_result = result;
 
         LyricMurekaId lyric_info;
         decltype(lyric_info.lyric) lyrics;
-        for (auto element : arr) {
+        for (auto element : json_result) {
             lyrics.emplace_back(parse_lyric(to_string(element["lyric"])));
         }
         lyric_info.lyric = lyrics;
         return lyric_info;
     }
 
+    int constexpr length(const char* str) {
+        return *str ? 1 + length(str + 1) : 0;
+    }
+
     std::string set_enc_key() {
-        mbedcrypto::rsa rsa;
-        rsa.import_public_key(mbedcrypto::buffer_t(R"xx(-----BEGIN RSA PUBLIC KEY-----
-MIGJAoGBAN+8Hz9MEOF+ARLXLniRbaUG7dV9oG6sauTwDdMBBnF4BXuqm6lO9uZl
-v7Kc7lZ95AgSScC+N2+YETg85tErrXRKLxL8FhicPW7AQSIrRZVBhBZfN9mNGI7V
-rRWP+LUATo5xf3FPyWKrfrAtWEgZYNTWLwnAtkLkluxwPsocZTdLAgMBAAE=
------END RSA PUBLIC KEY-----)xx"));
-        auto enc_key = rsa.encrypt(date::format(
-                "ALSONG_ANDROID_%4Y%2m%2d_%2H%2M%2S",
-                date::make_zoned(
-                        date::locate_zone("Etc/UTC"),
-                        std::chrono::system_clock::now())));
-        std::transform(enc_key.begin(), enc_key.end(), enc_key.begin(), ::toupper);
+        std::string enc_key;
+        auto key = reinterpret_cast<const unsigned char*>(ALKEY);
+        auto rsa = RSA_new();
+        if (rsa) {
+            auto bnN = BN_bin2bn(key, sizeof(key), nullptr);
+            auto bnE = BN_bin2bn(key, sizeof(key), nullptr);
+            RSA_set0_key(rsa, bnN, bnE, nullptr);
+            auto data = date::format(
+                    "ALSONG_ANDROID_%4Y%2m%2d_%2H%2M%2S",
+                    date::make_zoned(date::locate_zone("Etc/UTC"), std::chrono::system_clock::now())
+            );
+
+            auto len = RSA_size(rsa);
+            auto *encrypted = new unsigned char[len];
+
+            RSA_public_encrypt(
+                    static_cast<int>(data.length()),
+                    reinterpret_cast<const unsigned char *>(data.c_str()),
+                    encrypted,
+                    rsa,
+                    RSA_PKCS1_PADDING
+            );
+            enc_key = std::string(reinterpret_cast<char const*>(encrypted), len);
+            delete[] encrypted;
+            std::transform(enc_key.begin(), enc_key.end(), enc_key.begin(), ::toupper);
+            RSA_free(rsa);
+        }
         return enc_key;
     }
 
